a020bdae929e1152b3523bec8fa1e773f727058a
tmorris <tmorris@student.42.fr>
2022-05-05 17:29:20 +0200

    feat(backend): create database entities
    - User
    - Message
    - Channel
    - Karma

---
+++ backend/src/messages/message.entity.ts
@@ -0,0 +1,18 @@
+import { Channel } from 'src/channel/channel.entity';
+import { User } from 'src/users/user.entity';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Message {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  contest: string;
+
+  @ManyToOne(() => User)
+  sender: User;
+
+  @ManyToOne(() => Channel, (channel) => channel.messages)
+  channel: Channel;
+}
