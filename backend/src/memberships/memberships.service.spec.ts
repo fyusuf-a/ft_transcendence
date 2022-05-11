7a53e65c7884f0dbba0489e083b0b53e0e9c2fb9
tmorris <tmorris@student.42.fr>
2022-05-11 23:31:27 +0200

    test(backend): memberships: add service tests

--- backend/src/memberships/memberships.service.spec.ts
+++ backend/src/memberships/memberships.service.spec.ts
@@ -1,12 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { Channel } from 'src/channels/entities/channel.entity';
+import { EntityDoesNotExistError } from 'src/errors/entityDoesNotExist';
 import { User } from 'src/users/entities/user.entity';
-import { Membership } from './entities/membership.entity';
+import { Repository } from 'typeorm';
+import { Membership, MembershipRoleType } from './entities/membership.entity';
 import { MembershipsService } from './memberships.service';
 
 describe('MembershipsService', () => {
   let service: MembershipsService;
+  let channelsRepository: Repository<Channel>;
+  let usersRepository: Repository<User>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -18,19 +22,60 @@ describe('MembershipsService', () => {
         },
         {
           provide: getRepositoryToken(User),
-          useValue: jest.fn(),
+          useClass: Repository,
         },
         {
           provide: getRepositoryToken(Channel),
-          useValue: jest.fn(),
+          useClass: Repository,
         },
       ],
     }).compile();
 
     service = module.get<MembershipsService>(MembershipsService);
+    channelsRepository = module.get<Repository<Channel>>(
+      getRepositoryToken(Channel),
+    );
+    usersRepository = module.get<Repository<User>>(getRepositoryToken(User));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('create() throws without channel', async () => {
+    jest.spyOn(channelsRepository, 'findOne').mockResolvedValueOnce(undefined);
+    try {
+      await service.create({
+        channelId: 5,
+        userId: 10,
+        role: MembershipRoleType.PARTICIPANT,
+      });
+    } catch (error) {
+      if (error instanceof EntityDoesNotExistError) {
+        return expect(true).toBe(true);
+      } else {
+        throw error;
+      }
+    }
+    return expect(false).toBe(true);
+  });
+
+  it('create() throws without user', async () => {
+    const channel: Channel = new Channel();
+    channel.id = 5;
+    jest.spyOn(channelsRepository, 'findOne').mockResolvedValueOnce(channel);
+    jest.spyOn(usersRepository, 'findOne').mockResolvedValueOnce(undefined);
+    try {
+      await service.create({
+        channelId: 5,
+        userId: 10,
+        role: MembershipRoleType.PARTICIPANT,
+      });
+    } catch (error) {
+      if (error instanceof EntityDoesNotExistError) {
+        return expect(true).toBe(true);
+      }
+    }
+    return expect(false).toBe(true);
   });
 });
