588f714298a63c334a123ecf6baab6c7925b30c3
tmorris <tmorris@student.42.fr>
2022-05-23 17:34:24 +0200

    feat(auth): add env var to disable authentication

--- backend/src/auth/auth.jwt-auth.guard.ts
+++ backend/src/auth/auth.jwt-auth.guard.ts
@@ -1,11 +1,15 @@
 import { ExecutionContext, Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { Reflector } from '@nestjs/core';
 import { AuthGuard } from '@nestjs/passport';
 import { IS_PUBLIC_KEY } from './auth.public.decorator';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
-  constructor(private reflector: Reflector) {
+  constructor(
+    private reflector: Reflector,
+    private configService: ConfigService,
+  ) {
     super();
   }
 
@@ -14,6 +18,9 @@ export class JwtAuthGuard extends AuthGuard('jwt') {
       context.getHandler(),
       context.getClass(),
     ]);
+    if (this.configService.get<string>('DISABLE_AUTHENTICATION') === 'true') {
+      return true;
+    }
     if (isPublic) {
       return true;
     }
