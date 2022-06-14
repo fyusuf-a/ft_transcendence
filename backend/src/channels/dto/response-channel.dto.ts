ac27d688e5b5126faf4168275e5192c584736463
tmorris <tmorris@student.42.fr>
2022-06-14 14:30:54 +0200

    test(e2e): remove unused OmitType definition

--- backend/src/channels/dto/response-channel.dto.ts
+++ backend/src/channels/dto/response-channel.dto.ts
@@ -1,4 +1,4 @@
-import { OmitType, PickType } from '@nestjs/swagger';
+import { PickType } from '@nestjs/swagger';
 import { ChannelDto } from './channel.dto';
 
 export class ResponseChannelDto extends PickType(ChannelDto, [
