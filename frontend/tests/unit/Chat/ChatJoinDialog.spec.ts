45806482619041e8a9a05cd391159385d298968f
tmorris <tmorris@student.42.fr>
2022-07-04 16:09:31 +0200

    test: create test for ChatJoinDialog.vue

---
+++ frontend/tests/unit/Chat/ChatJoinDialog.spec.ts
@@ -0,0 +1,16 @@
+import { expect } from "chai";
+import { shallowMount } from "@vue/test-utils";
+import ChannelJoinDialog from "@/components/Chat/ChannelJoinDialog.vue";
+
+describe("ChannelJoinDialog.vue", () => {
+  it("renders props", async () => {
+    const joinableChannels: any[] = [];
+    const wrapper = shallowMount(ChannelJoinDialog, {
+      propsData: { joinableChannels },
+    });
+    expect(wrapper.text()).to.include("Join Channel");
+
+    await wrapper.setData({ action: "Create" });
+    expect(wrapper.text()).to.include("Create Channel");
+  });
+});
