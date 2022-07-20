bc52201d20eb4db07ed06513f93beb3a563818ff
louie <mdesfont@student.42.fr>
2022-07-20 17:21:13 +0200

    feat(backend): implement oauth

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,23 +1,59 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Get, Res } from '@nestjs/common';
 import { ApiBody, ApiTags } from '@nestjs/swagger';
 import { Public } from './auth.public.decorator';
 import { AuthService } from './auth.service';
 import { LoginUserDto } from '@dtos/auth';
 import { UnauthorizedException } from '@nestjs/common';
+import { get } from 'http';
+import { Response } from '@nestjs/common';
 
 @ApiTags('auth')
 @Controller('auth')
 export class AuthController {
-  constructor(private authService: AuthService) {}
+  // constructor(private authService: AuthService) {}
 
-  @ApiBody({ type: LoginUserDto })
-  @Public()
-  @Post('login')
-  async login(@Body() userDto: LoginUserDto) {
-    try {
-      return await this.authService.login(userDto);
-    } catch (EntityNotFoundError) {
-      throw new UnauthorizedException('Username is incorrect');
-    }
+  // @ApiBody({ type: LoginUserDto })
+  // @Public()
+  // @Post('login')
+  // async login(@Body() userDto: LoginUserDto) {
+  //   try {
+  //     return await this.authService.login(userDto);
+  //   } catch (EntityNotFoundError) {
+  //     throw new UnauthorizedException('Username is incorrect');
+  //   }
+  // }
+
+    
+
+  /*
+  * This is the road the user will visit to authenticate
+  */
+
+  @Get('login')
+  login() {
+    return;
   }
+
+  /*
+  * This is the redirect url the oauth2 provider will call
+  */
+
+  @Get('redirect')
+  redirect(@Res() res: Response) {
+    res.send(200);
+  }
+
+  /*
+  * Retrieve the auth status
+  */
+
+  @Get('status')
+  status() { }
+
+  /*
+  * Logging the user out
+  */
+
+  @Get('logout')
+  logout() { }
 }
