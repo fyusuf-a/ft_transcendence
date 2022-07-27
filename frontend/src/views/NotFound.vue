cbdcfcd8d94014edd06508bf43ca5bf3f8f80534
louie <mdesfont@student.42.fr>
2022-07-27 20:34:55 +0200

    refactor(front): corrected style and syntax errors, in particular to have TS instead of JS

--- frontend/src/views/NotFound.vue
+++ frontend/src/views/NotFound.vue
@@ -2,38 +2,43 @@
   <v-container>
     <div class="content">
       <h1>Uh oh. Error 404: Page Not Found</h1>
-      <br>
-      <p>We are really sorry, but the page you're looking for doesn't seem to exist.
-      <br>Do you want to return to your <router-link to="/profile">profile page</router-link> or <router-link to="/game">play a match</router-link>?
-      <br>Anyway, it's dangerous to go alone, take a cute little cat with you.</p>
+      <br />
+      <p>
+        We are really sorry, but the page you're looking for doesn't seem to
+        exist. <br />Do you want to return to your
+        <router-link to="/profile">profile page</router-link> or
+        <router-link to="/game">play a match</router-link>? <br />Anyway, it's
+        dangerous to go alone, take a cute little cat with you.
+      </p>
       <v-card
-	  		outlined color="transparent"
-	  		:loading="loading"
-	  		class="mx-auto my-12"
-	  	>
-	  		<v-img :src="errorPicture"></v-img>
-	  	</v-card>
-	  	<p class="imgSrc">
-	  		<a
-	  			href="https://pixabay.com/fr/photos/cat-animal-de-compagnie-animal-98359/"
-	  			target="_blank"
-	  		>
-	  			image source
-	  		</a>
-	  	</p>
+        outlined
+        color="transparent"
+        :loading="loading"
+        class="mx-auto my-12"
+      >
+        <v-img :src="errorPicture"></v-img>
+      </v-card>
+      <p class="imgSrc">
+        <a
+          href="https://pixabay.com/fr/photos/cat-animal-de-compagnie-animal-98359/"
+          target="_blank"
+        >
+          image source
+        </a>
+      </p>
     </div>
   </v-container>
 </template>
 
 <script lang="ts">
-export default {
-  data () {
+import Vue from 'vue';
+export default Vue.extend({
+  data() {
     return {
-      errorPicture: require('@/assets/images/40cat.png')
+      errorPicture: require('@/assets/images/40cat.png'),
     };
-  }
-
-}
+  },
+});
 </script>
 
 <style scoped>
@@ -42,18 +47,16 @@ export default {
 }
 
 .imgSrc {
-	text-align: right;
-	padding-top: 75px;
-	color: black;
+  text-align: right;
+  padding-top: 75px;
+  color: black;
 }
 
 a {
   text-decoration: none;
 }
 
-a:visited {
-  /* color: black; */
-}
+a:visited {}
 
 a:hover {
   background: teal;
