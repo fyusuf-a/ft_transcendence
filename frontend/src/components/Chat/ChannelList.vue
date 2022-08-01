887177e60cf733a00a30ee6e99de14d53a0b96b3
tmorris <tmorris@student.42.fr>
2022-08-01 15:40:03 +0200

    fix: get ChannelList working with tooltips

--- frontend/src/components/Chat/ChannelList.vue
+++ frontend/src/components/Chat/ChannelList.vue
@@ -1,25 +1,52 @@
 <template>
-	<v-card>
-		<v-container>
-			<v-row no-gutters align="center">
-				<v-col cols="10">{{ title }}</v-col>
-				<v-col cols="1">
-					<channel-join-dialog @channel-join-event="handleChannelJoin"
-						:joinableChannels="getJoinableChannels"></channel-join-dialog>
-				</v-col>
-				<v-spacer></v-spacer>
-			</v-row>
-			<v-row>
-				<v-divider></v-divider>
-			</v-row>
-			<v-row>
-				<v-col>
-					<v-list :items="channels" item-title="name" item-value="id" @update:selected="handleChannelSelection">
-					</v-list>
-				</v-col>
-			</v-row>
-		</v-container>
-	</v-card>
+  <v-card>
+    <v-container>
+      <v-row no-gutters align="center">
+        <v-col cols="10">{{ title }}</v-col>
+        <v-col cols="1">
+          <channel-join-dialog
+            @channel-join-event="handleChannelJoin"
+            :joinableChannels="getJoinableChannels"
+          >
+          </channel-join-dialog>
+        </v-col>
+        <v-spacer></v-spacer>
+      </v-row>
+      <v-row>
+        <v-divider></v-divider>
+      </v-row>
+      <v-row>
+        <v-col>
+          <v-list density="compact">
+            <v-list-item-group color="primary">
+              <v-list-item
+                v-for="(item, i) in channels"
+                :key="i"
+                active-color="primary"
+                @click="() => handleChannelSelection(item)"
+              >
+                <v-tooltip bottom>
+                  <template v-slot:activator="{ props: tooltip }">
+                    <v-list-item-icon v-bind="tooltip">
+                      <v-icon
+                        v-if="unreadChannels.has(item.id)"
+                        color="secondary"
+                        >{{ unreadMarker }}</v-icon
+                      >
+                    </v-list-item-icon>
+                    <v-list-item-content v-bind="tooltip">
+                      <v-list-item-title v-text="item.name"></v-list-item-title>
+                    </v-list-item-content>
+                  </template>
+                  <span>{{ item.name }}</span>
+                </v-tooltip>
+              </v-list-item>
+            </v-list-item-group>
+          </v-list>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-card>
 </template>
 
 <script lang="ts">
@@ -28,53 +55,47 @@ import { defineComponent, PropType } from 'vue';
 import ChannelJoinDialog from './ChannelJoinDialog.vue';
 
 interface DataReturnType {
-	title: string;
-	unreadMarker: string;
+  title: string;
+  unreadMarker: string;
 }
 
 export default defineComponent({
-	props: {
-		channels: {
-			type: Array as () => Array<ChannelDto>,
-			required: true,
-		},
-		allChannels: {
-			type: Map as PropType<Map<number, ChannelDto>>,
-			required: true,
-		},
-		unreadChannels: {
-			type: Set, //Set<number>,
-			required: true,
-		},
-	},
-	data(): DataReturnType {
-		return {
-			title: 'Channels',
-			unreadMarker: 'mdi-new-box',
-		};
-	},
-	components: { 'channel-join-dialog': defineComponent(ChannelJoinDialog) },
-	methods: {
-		async handleChannelSelection(channelIds: string[]) {
-			const channelId: number = +channelIds[0];
-			console.log('Handling a channel selection');
-			if (channelId) {
-				console.log(channelId);
-				// this.$emit('channel-select-event', this.channels[channelId]);
-			} else {
-				console.log('No channel selected');
-			}
-		},
-		handleChannelJoin(channelId: number) {
-			this.$emit('channel-join-event', channelId);
-		},
-	},
-	computed: {
-		getJoinableChannels(): ChannelDto[] {
-			return Array.from(this.allChannels.values()).filter(
-				(chan) => !this.channels.includes(chan),
-			) as ChannelDto[];
-		},
-	},
+  props: {
+    channels: {
+      type: Array as () => Array<ChannelDto>,
+      required: true,
+    },
+    allChannels: {
+      type: Map as PropType<Map<number, ChannelDto>>,
+      required: true,
+    },
+    unreadChannels: {
+      type: Set, //Set<number>,
+      required: true,
+    },
+  },
+  data(): DataReturnType {
+    return {
+      title: 'Channels',
+      unreadMarker: 'mdi-new-box',
+    };
+  },
+  components: { 'channel-join-dialog': defineComponent(ChannelJoinDialog) },
+  methods: {
+    async handleChannelSelection(channel: ChannelDto) {
+      console.log(`Handling a channel selection: ${channel.id}`);
+      this.$emit('channel-select-event', channel);
+    },
+    handleChannelJoin(channelId: number) {
+      this.$emit('channel-join-event', channelId);
+    },
+  },
+  computed: {
+    getJoinableChannels(): ChannelDto[] {
+      return Array.from(this.allChannels.values()).filter(
+        (chan) => !this.channels.includes(chan),
+      ) as ChannelDto[];
+    },
+  },
 });
 </script>
