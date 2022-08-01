b595d530180311e84170e013cfd0df3dd64cde2f
fyusuf-a <fyusuf-a@student.42.fr>
2022-08-01 15:36:00 +0200

    fix(frontend): vite resolves every file

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import path from 'path';
 import checker from 'vite-plugin-checker';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -17,20 +17,21 @@ export default defineConfig({
     }),
   ],
   resolve: {
-    preserveSymlinks: true,
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-      '@dtos': path.resolve(__dirname, 'src/dtos'),
-      '@nestjs/swagger': path.resolve('../src/shims-nestjs.d'),
-    },
-  },
-  optimizeDeps: {
-    exclude: [
-      'class-transformer/storage',
-      '@nestjs/platform-express',
-      '@nestjs/microservices',
-      '@nestjs/websockets/socket-module',
-      'cache-manager',
+    alias: [
+      {
+        find: /^@dtos(.*)$/,
+        replacement: `${path.resolve('../dtos/')}$1`,
+      },
+      {
+        find: /^@\/(.*)$/,
+        replacement: `${path.resolve('./src/')}/$1`,
+      },
+      {
+        find: '@nestjs/swagger',
+        replacement: './src/shims-nestjs.d',
+      },
     ],
   },
+  clearScreen: false,
+  logLevel: 'error',
 });
