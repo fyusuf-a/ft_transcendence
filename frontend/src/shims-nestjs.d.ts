5caedafecfdece429414fe1539c3200e214db8e5
fyusuf-a <fyusuf-a@student.42.fr>
2022-08-02 18:19:21 +0200

    fix(frontend): there remains no typescript error

--- frontend/src/shims-nestjs.d.ts
+++ frontend/src/shims-nestjs.d.ts
@@ -1,6 +1,24 @@
-export function ApiProperty() {
-  return;
+export interface ApiPropertyOptions {
+  description?: string;
+  enumName?: string;
+  enum?: unknown[] | Record<string, unknown>;
+  type?: Type<unknown> | string | Record<string, unknown>;
+  examples?: unknown[] | Record<string, unknown>;
+  isArray?: boolean;
+  minimum?: number;
+  maximum?: number;
+  default?: unknown;
+  format?: string;
 }
-export function ApiPropertyOptional() {
-  return;
+
+// eslint-disable-next-line
+export function ApiProperty(_: ApiPropertyOptions = {}) {
+  // eslint-disable-next-line
+  return function (_, _: string | symbol) {};
+}
+
+// eslint-disable-next-line
+export function ApiPropertyOptional(_: ApiPropertyOptions = {}) {
+  // eslint-disable-next-line
+  return function (_, _: string | symbol) {}
 }
