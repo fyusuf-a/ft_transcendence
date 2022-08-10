98d783c09d37f5809abc132301a8325f6bd30884
tmorris <tmorris@student.42.fr>
2022-08-10 17:53:08 +0200

    fix: make pong appear

--- frontend/src/components/Game/GameWindow.vue
+++ frontend/src/components/Game/GameWindow.vue
@@ -1,38 +1,142 @@
 <template>
-  <canvas ref="pong" id="pong" width="640px" height="480px"></canvas>
+  <div>
+    <div>
+      <v-btn @click="clear">Clear Screen</v-btn> {{ x }}, {{ y }}
+      <v-btn @click="startGame">Start Game</v-btn>
+    </div>
+    <canvas
+      ref="pong"
+      id="pong"
+      width="640"
+      height="480"
+      @mousemove="draw"
+    ></canvas>
+    <canvas
+      id="background"
+      width="640"
+      height="480"
+      style="visibility: hidden"
+    ></canvas>
+    <canvas
+      id="paddle"
+      width="10"
+      height="100"
+      style="visibility: hidden"
+    ></canvas>
+    <canvas
+      id="ball"
+      width="13"
+      height="13"
+      style="visibility: hidden"
+    ></canvas>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import { Pong } from './src/pong';
 
-export default defineComponent({
-  data: function () {
-    return {};
-  },
-  updated() {
-    // const background = new Image();
-    // background.src = 'src/assets/images/background.png';
+interface DataReturnTypes {
+  ctx: CanvasRenderingContext2D | null;
+  x: number;
+  y: number;
+  pong: Pong | null;
+  pongCanvas: HTMLCanvasElement | null;
+  backgroundCanvas: HTMLCanvasElement | null;
+  ballCanvas: HTMLCanvasElement | null;
+  paddleCanvas: HTMLCanvasElement | null;
+}
 
-    // // const canvas = document.getElementById('pong') as HTMLCanvasElement;
-    // const canvas = this.$refs.pong as HTMLCanvasElement;
-    // const ctx: CanvasRenderingContext2D = canvas.getContext(
-    //   '2d',
-    // ) as CanvasRenderingContext2D;
-    // ctx.beginPath();
-    // ctx.lineWidth = 5;
-    // ctx.lineCap = "round";
-    // ctx.strokeStyle = "green";
-    // ctx.rect(20, 20, 150, 150);
-    // ctx.fill();
-    const pong = new Pong();
-    pong.start();
+export default defineComponent({
+  data: function (): DataReturnTypes {
+    return {
+      ctx: null,
+      x: 0,
+      y: 0,
+      pong: null,
+      pongCanvas: null,
+      backgroundCanvas: null,
+      ballCanvas: null,
+      paddleCanvas: null,
+    };
+  },
+  methods: {
+    startGame() {
+      if (!this.pong) {
+        this.pong = new Pong(
+          this.pongCanvas,
+          this.ballCanvas,
+          this.backgroundCanvas,
+          this.paddleCanvas,
+        );
+      }
+      this.pong.start();
+    },
+    clear() {
+      const canvas = this.$refs.pong as HTMLCanvasElement;
+      if (canvas) console.log('canvas loaded');
+      console.log(canvas);
+      const ctx: CanvasRenderingContext2D = canvas.getContext(
+        '2d',
+      ) as CanvasRenderingContext2D;
+      if (ctx) console.log('context loaded');
+      console.log(ctx);
+      ctx.fillStyle = 'white';
+      ctx.fillRect(0, 0, 1500, 1000);
+    },
+    draw(e: MouseEvent) {
+      this.drawLine(this.x, this.y, e.offsetX, e.offsetY);
+      this.x = e.offsetX;
+      this.y = e.offsetY;
+    },
+    drawLine(x1: number, y1: number, x2: number, y2: number) {
+      let ctx = this.ctx;
+      if (!ctx) return;
+      ctx.beginPath();
+      ctx.strokeStyle = 'black';
+      ctx.lineWidth = 1;
+      ctx.moveTo(x1, y1);
+      ctx.lineTo(x2, y2);
+      ctx.stroke();
+      // ctx.closePath();
+    },
+  },
+  mounted() {
+    console.log('mounted');
+    this.pongCanvas = document.getElementById('pong') as HTMLCanvasElement;
+    this.backgroundCanvas = document.getElementById(
+      'background',
+    ) as HTMLCanvasElement;
+    this.ballCanvas = document.getElementById('ball') as HTMLCanvasElement;
+    this.paddleCanvas = document.getElementById('paddle') as HTMLCanvasElement;
+    this.ctx = this.pongCanvas.getContext('2d') as CanvasRenderingContext2D;
   },
 });
 </script>
 
 <style scoped>
 #pong {
+  border: solid black 1px;
+  width: 640px;
+  height: 480px;
+  margin-left: 10px;
+}
+
+#ball {
+  border: solid black 1px;
+  width: 640px;
+  height: 480px;
+  margin-left: 10px;
+}
+
+#paddle {
+  border: solid black 1px;
+  width: 640px;
+  height: 480px;
+  margin-left: 10px;
+}
+
+#background {
   border: solid black 1px;
   width: 640px;
   height: 480px;
--- frontend/src/components/Game/src/background.ts
+++ frontend/src/components/Game/src/background.ts
@@ -4,13 +4,18 @@ background.src = 'src/assets/images/background.png';
 class Background {
   img_data: ImageData;
 
-  constructor() {
-    const canvas = document.getElementById('background') as HTMLCanvasElement;
-    const ctx: CanvasRenderingContext2D = canvas.getContext(
+  constructor(backgroundCanvas: HTMLCanvasElement | null) {
+    const canvas = backgroundCanvas;
+    const ctx: CanvasRenderingContext2D = canvas?.getContext(
       '2d',
     ) as CanvasRenderingContext2D;
     ctx.drawImage(background, 0, 0);
-    this.img_data = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    this.img_data = ctx.getImageData(
+      0,
+      0,
+      backgroundCanvas?.width ? backgroundCanvas.width : 0,
+      backgroundCanvas?.height ? backgroundCanvas.height : 0,
+    );
   }
   render(ctx: CanvasRenderingContext2D) {
     ctx.putImageData(this.img_data, 0, 0);
