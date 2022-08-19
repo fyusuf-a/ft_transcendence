d17ca15ae43f855eea4797604e49fd771968164e
louie <mdesfont@student.42.fr>
2022-08-19 18:22:48 +0200

    feat(front): the user can change their username

--- frontend/src/components/Profile/MyProfile.vue
+++ frontend/src/components/Profile/MyProfile.vue
@@ -1,7 +1,6 @@
 <template>
-  <v-card class="pa-2 ml-15 mr-15 mt-5" width="84%" >  
-    <!-- <v-card-title>{{ username() }}</v-card-title> -->
-    <v-card-title>louie</v-card-title>
+  <v-card class="pa-2 ml-15 mr-15 mt-5" width="84%" >
+    <v-card-title>{{ username() }}</v-card-title>
   
       <v-container fluid>
         <v-row dense>
@@ -16,8 +15,63 @@
               <v-btn color="deep-purple lighten-2" text @click="reserve">
                 Change picture
               </v-btn>
-              <v-btn color="deep-purple lighten-2" text @click="reserve">
+              
+              
+              
+              <!-- <v-btn color="deep-purple lighten-2" text @click="changeUsername">
                 Change username
+              </v-btn> -->
+              <v-btn
+                color="primary"
+              >
+                Change username
+                <v-dialog
+                  v-model="dialog"
+                  activator="parent"
+                >
+                  <v-card>
+                    <v-form
+                      ref="form"
+                      v-model="valid"
+                      lazy-validation
+                    >
+                      <v-text-field
+                        v-model="name"
+                        :counter="15"
+                        :rules="nameRules"
+                        label="Name"
+                        required
+                      ></v-text-field>
+
+                      <v-checkbox
+                        v-model="checkbox"
+                        :rules="[(v: string) => !!v || 'You must agree to continue!']"
+                        label="Do you agree?"
+                        required
+                      ></v-checkbox>
+
+                      <v-btn
+                        :disabled="!valid"
+                        color="success"
+                        class="mr-4"
+                        @click="validate"
+                      >
+                        Validate
+                      </v-btn>
+
+                      <v-btn
+                        color="error"
+                        class="mr-4"
+                        @click="reset"
+                      >
+                        Reset Form
+                      </v-btn>
+                    </v-form>
+                    <v-card-actions>
+                      <v-btn color="primary" block @click="dialog = false">I changed my mind</v-btn>
+                    </v-card-actions>
+                  </v-card>
+                </v-dialog>
               </v-btn>
             </v-card-actions>
           </v-col>
@@ -39,18 +93,68 @@ import { defineComponent } from 'vue';
 import { mapGetters } from 'vuex';
 import MyStatistics from '@/components/Profile/MyStatistics.vue';
 import MyLevel from '@/components/Profile/MyLevel.vue';
+import axios from 'axios';
 
 export default defineComponent({
+  data: () => ({
+    dialog: false,
+    valid: true,
+    name: '',
+    nameRules: [
+      (v: string) => !!v || 'Name is required',
+      (v: string) => (v && v.length <= 15) || 'Name must be less than 15 characters',
+    ],
+    select: null,
+    checkbox: false,
+  }),
   components: {
     'my-statistics': MyStatistics,
     'my-level': MyLevel,
   },
   methods: {
-    ...mapGetters(['username', 'avatar']),
+    ...mapGetters(['username', 'avatar', 'id']),
     reserve() {
       // TODO: remove and replace with 2 methods: one to change the username, one to change the picture
     },
+
+    validate () {
+      this.changeUsername(this.name);
+      (this.$refs.form as any).validate();
+      this.dialog = false;
+      (this.$refs.form as any).reset();
+      this.checkbox = false;
+    },
+    reset () {
+      (this.$refs.form as any).reset();
+      this.checkbox = false;
+    },
+    async changeUsername(name: string) {
+       
+const response = await axios.get('/users/' + this.id());
+console.log(response.data);
+console.log(response.data.username);
+      
+      const data = {
+        username: name,
+    	};
+
+      await axios.patch('/users/' + this.id(), data)
+        .then(response => {
+          console.log(response);
+        })
+        .catch( (error) => {
+          console.log(error.response);
+        })
+      ;
+
+      const response2 = await axios.get('http://localhost:8080/users/' + this.id());
+console.log(response2.data);
+console.log(response2.data.username);
+        
+        this.$store.commit('setUsername', response2.data.username);
+    },
   },
+
   created() {
     if (this.avatar() !== undefined) return;
     this.$store.dispatch('getAvatar');
