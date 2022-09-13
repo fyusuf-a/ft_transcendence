8404975ac17d221baa1e3c75ac14727cb72d9c16
tmorris <tmorris@student.42.fr>
2022-09-13 14:57:28 +0200

    feat(game): create dto/game/index.ts

--- backend/src/game/game.ts
+++ backend/src/game/game.ts
@@ -1,7 +1,6 @@
 import { Server, Socket } from 'socket.io';
-import { CreateGameDto } from '@dtos/game/create-game.dto';
+import { CreateGameDto, StateDto } from '@dtos/game';
 import { GameState } from './game-state';
-
 const FRAMERATE = 30;
 
 export class Game {
@@ -27,7 +26,7 @@ export class Game {
 
   updateServer() {
     this.state.update();
-    const state = {
+    const state: StateDto = {
       ball: { x: this.state.ball.x, y: this.state.ball.y },
       player1: { x: this.state.players[0].x, y: this.state.players[0].y },
       player2: { x: this.state.players[1].x, y: this.state.players[1].y },
--- dtos/game/state.dto.ts
+++ dtos/game/state.dto.ts
@@ -2,7 +2,6 @@ import { BallDto } from './ball.dto';
 import { PlayerDto } from './player.dto';
 
 export interface StateDto {
-  gameId: number;
   ball: BallDto;
   player1: PlayerDto;
   player2: PlayerDto;
