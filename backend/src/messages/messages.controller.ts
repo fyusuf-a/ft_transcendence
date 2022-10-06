1e73218a1182be4eb9b4c26fdff03f0944b815c9
fyusuf-a <fyusuf-a@student.42.fr>
2022-10-06 09:43:00 +0200

    feat(backend): adds authorization to the Message controller

--- backend/src/messages/messages.controller.ts
+++ backend/src/messages/messages.controller.ts
@@ -9,6 +9,7 @@ import {
   Param,
   Post,
   Query,
+  ForbiddenException,
 } from '@nestjs/common';
 import {
   ApiBadRequestResponse,
@@ -22,52 +23,105 @@ import {
   QueryMessageDto,
   ResponseMessageDto,
 } from '@dtos/messages';
-import { DeleteResult, EntityNotFoundError } from 'typeorm';
+import { DeleteResult } from 'typeorm';
 import { MessagesService } from './messages.service';
+import { AuthUser, User } from 'src/auth/auth-user.decorator';
+import { MembershipsService } from 'src/memberships/memberships.service';
+
+type Inability = {
+  muted: boolean | undefined;
+  banned: boolean | undefined;
+};
 
 @ApiBearerAuth()
 @ApiTags('messages')
 @Controller('messages')
 export class MessagesController {
-  constructor(private readonly messagesService: MessagesService) {}
+  constructor(
+    private readonly messagesService: MessagesService,
+    private readonly membershipsService: MembershipsService,
+  ) {}
+
+  async #isUserCapableInChannel(
+    user: User,
+    channelId: string,
+    inability: Inability,
+  ) {
+    const memberships = await this.membershipsService.findAll({
+      channel: channelId,
+      user: user.id.toString(),
+    });
+    if (memberships.length === 0) throw new ForbiddenException();
+    const membership = memberships[0];
+    const date = new Date();
+    if (inability.banned === false && date < membership.bannedUntil) {
+      throw new ForbiddenException();
+    }
+    if (inability.muted === false && date < membership.mutedUntil) {
+      throw new ForbiddenException();
+    }
+  }
 
   @Get()
   async findAll(
-    @Query() query?: QueryMessageDto,
+    @AuthUser() user: User,
+    @Query() query: QueryMessageDto,
     @Query() pageOptions?: PageOptionsDto,
   ): Promise<PageDto<ResponseMessageDto>> {
+    await this.#isUserCapableInChannel(user, query.channel, {
+      banned: false,
+      muted: undefined,
+    });
     if (query && query.sender)
       return this.messagesService.findAll(query, pageOptions);
     else return this.messagesService.findAllWithBlocks(query, pageOptions);
   }
 
+  // TODO: remove senderId from CreateMessageDto
   @Post()
   @ApiBadRequestResponse({ description: 'Bad Request' })
   @ApiResponse({ status: 500, description: 'Record could not be created.' })
   async create(
+    @AuthUser() user: User,
     @Body() createMessageDto: CreateMessageDto,
   ): Promise<ResponseMessageDto> {
-    try {
-      const ret = await this.messagesService.create(createMessageDto);
-      return ret;
-    } catch (err) {
-      if (err == 'Unauthorized') {
-        throw new HttpException(
-          'You are banned or muted on this channel.',
-          HttpStatus.UNAUTHORIZED,
-        );
-      }
-    }
+    if (user.id !== createMessageDto.senderId) throw new ForbiddenException();
+    await this.#isUserCapableInChannel(
+      user,
+      createMessageDto.channelId.toString(),
+      {
+        banned: false,
+        muted: false,
+      },
+    );
+    return await this.messagesService.create(createMessageDto);
   }
 
   @Get(':id')
   @ApiResponse({ status: 404, description: 'Record not found.' })
-  async findOne(@Param('id') id: string): Promise<ResponseMessageDto> {
-    return await this.messagesService.findOne(+id);
+  async findOne(
+    @AuthUser() user: User,
+    @Param('id') id: string,
+  ): Promise<ResponseMessageDto> {
+    const message = await this.messagesService.findOne(+id);
+    await this.#isUserCapableInChannel(user, message.channelId.toString(), {
+      banned: false,
+      muted: undefined,
+    });
+    return message;
   }
 
   @Delete(':id')
-  removeById(@Param('id') id: string): Promise<DeleteResult> {
+  async removeById(
+    @AuthUser() user: User,
+    @Param('id') id: string,
+  ): Promise<DeleteResult> {
+    const message = await this.messagesService.findOne(+id);
+    if (user.id !== message.senderId) throw new ForbiddenException();
+    await this.#isUserCapableInChannel(user, message.channelId.toString(), {
+      banned: false,
+      muted: undefined,
+    });
     return this.messagesService.remove(+id);
   }
 }
