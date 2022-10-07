e240007f9068f6225ab803b8b9627b982ea948fb
fyusuf-a <fyusuf-a@student.42.fr>
2022-10-07 02:07:37 +0200

    feat(backend): feat(backend): adds authorization to the Channel controller

--- backend/src/channels/channels.controller.ts
+++ backend/src/channels/channels.controller.ts
@@ -25,18 +25,30 @@ import {
 } from '@dtos/channels';
 import { DeleteResult, UpdateResult } from 'typeorm';
 import { ChannelsService } from './channels.service';
+import { AuthUser, User } from 'src/auth/auth-user.decorator';
+import { Channel, ChannelType } from './entities/channel.entity';
+import { CaslAbilityFactory, Action } from 'src/casl/casl-ability.factory';
+import { MembershipsService } from 'src/memberships/memberships.service';
+import { MembershipRoleType } from 'src/memberships/entities/membership.entity';
+import { EntityNotFoundError } from 'typeorm';
 
 @ApiBearerAuth()
 @ApiTags('channels')
 @Controller('channels')
 export class ChannelsController {
-  constructor(private readonly channelsService: ChannelsService) {}
+  constructor(
+    private readonly channelsService: ChannelsService,
+    private readonly membershipsService: MembershipsService,
+    private readonly abilityFactory: CaslAbilityFactory,
+  ) {}
 
   @ApiBody({ type: CreateChannelDto })
   @Post()
   async create(
+    @AuthUser() user: User,
     @Body() createChannelDto: CreateChannelDto,
   ): Promise<ResponseChannelDto> {
+    await this.abilityFactory.checkAbility(user, Action.Create, Channel);
     let ret: ResponseChannelDto;
 
     try {
@@ -48,30 +60,64 @@ export class ChannelsController {
     return ret;
   }
 
+  // TODO: ignore the PRIVATE and DIRECT channels
   @Get()
   async findAll(
+    @AuthUser() user: User,
     @Query() query?: QueryChannelDto,
     @Query() pageOptions?: PageOptionsDto,
   ): Promise<PageDto<ResponseChannelDto>> {
+    await this.abilityFactory.checkAbility(user, Action.Read, Channel);
     return await this.channelsService.findAll(query, pageOptions);
   }
 
   @Get(':id')
   @ApiNotFoundResponse({ description: 'Channel Not Found' })
-  async findOne(@Param('id') id: string): Promise<ResponseChannelDto> {
-    return this.channelsService.findOne(+id);
+  async findOne(
+    @AuthUser() user: User,
+    @Param('id') id: string,
+  ): Promise<ResponseChannelDto> {
+    const channel = await this.channelsService.findOne(+id);
+    if (
+      channel.type === ChannelType.PRIVATE ||
+      channel.type === ChannelType.DIRECT
+    ) {
+      try {
+        await this.membershipsService.isUserCapableInChannel(user, id, {
+          banned: undefined,
+          muted: undefined,
+        });
+      } catch (e) {
+        throw new EntityNotFoundError(Channel, '');
+      }
+    }
+    return channel;
   }
 
   @Patch(':id')
   update(
+    @AuthUser() user: User,
     @Param('id') id: string,
     @Body() updateChannelDto: UpdateChannelDto,
   ): Promise<UpdateResult> {
+    this.membershipsService.hasUserRoleInChannel(
+      user,
+      MembershipRoleType.OWNER,
+      id,
+    );
     return this.channelsService.update(+id, updateChannelDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string): Promise<DeleteResult> {
+  remove(
+    @AuthUser() user: User,
+    @Param('id') id: string,
+  ): Promise<DeleteResult> {
+    this.membershipsService.hasUserRoleInChannel(
+      user,
+      MembershipRoleType.OWNER,
+      id,
+    );
     return this.channelsService.remove(+id);
   }
 }
