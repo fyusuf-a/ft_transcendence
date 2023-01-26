eaca45ef02f9046a73ca917b8b0ca6b94dc698a5
fyusuf-a <fyusuf-a@student.42.fr>
2023-01-26 17:55:34 +0100

    feat(frontend): enables hot module reload

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,7 +7,7 @@ import vuetify from 'vite-plugin-vuetify';
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    hmr: false,
+    hmr: true,
     proxy: {
       '/socket.io': {
         target: 'ws://backend:8080',
